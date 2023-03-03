548f426edcc672434057c13b0d2871f6112e12bf
plastikfan <plastikfan@outlook.com>
2023-03-03 19:34:00 +0000

    feat(greet): add message (#7)

--- greeter.go
+++ greeter.go
@@ -6,8 +6,11 @@ type Greeter struct {
 	Forename string
 	Surname  string
 	Title    string
+	Message  string
 }
 
 func (g *Greeter) Hello() string {
-	return fmt.Sprintf("---> Hello %v. %v %v", g.Title, g.Forename, g.Surname)
+	return fmt.Sprintf("---> Hello %v. %v %v: %v",
+		g.Title, g.Forename, g.Surname, g.Message,
+	)
 }
