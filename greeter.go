4648a4d8ad9526e184782a36734e7a8fc44c07b3
plastikfan <plastikfan@outlook.com>
2023-03-06 13:50:51 +0000

    feat(greet): add age (#8)

--- greeter.go
+++ greeter.go
@@ -7,10 +7,15 @@ type Greeter struct {
 	Surname  string
 	Title    string
 	Message  string
+	Age      uint
 }
 
 func (g *Greeter) Hello() string {
-	return fmt.Sprintf("---> Hello %v. %v %v: %v",
-		g.Title, g.Forename, g.Surname, g.Message,
+	prompt := "What's your poison"
+	if g.Age <= 18 {
+		prompt = "Milk and cookies for you"
+	}
+	return fmt.Sprintf("---> Hello %v. %v %v: %v == %v",
+		g.Title, g.Forename, g.Surname, g.Message, prompt,
 	)
 }
